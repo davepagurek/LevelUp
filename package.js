85120fac0956f430aec934a74097187922ce7248
Dave Pagurek <dave@davepagurek.com>
2016-02-15 08:37:12 -0800

    Release script

---
+++ package.js
@@ -0,0 +1,19 @@
+var NwBuilder = require('nw-builder');
+var nw = new NwBuilder({
+  files: ['./package.json', './dist/**/*', './node_modules/**/*', './bower_components/**/*', './css/**/*', '!./node_modules/nw-builder/**/*', '!./node_modules/nw/**/*'], // use the glob format
+  platforms: ['win32'],
+  version: '0.12.3',
+  buildDir: './build',
+  zip: false
+});
+
+//Log stuff you want
+
+nw.on('log',  console.log);
+
+// Build returns a promise
+nw.build().then(function () {
+   console.log('all done!');
+}).catch(function (error) {
+    console.error(error);
+});
