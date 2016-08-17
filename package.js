bc4b3ae83ab8fba23ea532d7fd4efee31754ca66
Dave Pagurek <dave@davepagurek.com>
2016-08-17 19:28:18 -0400

    Fix dependencies and packaging

--- package.js
+++ package.js
@@ -1,7 +1,8 @@
 var NwBuilder = require('nw-builder');
 var nw = new NwBuilder({
-  files: ['index.html', './package.json', './dist/**/*', './node_modules/**/*', './bower_components/**/*', './css/**/*', '!./node_modules/nw-builder/**/*', '!./node_modules/nw/**/*'], // use the glob format
-  platforms: ['osx64'],
+  files: ['index.html', './package.json', './dist/**/*', './bower_components/**/*', './css/**/*', '!./node_modules/nw-builder/**/*', '!./node_modules/nw/**/*', '!./node_modules/babel*/**/*', '!./node_modules/webpack/**/*', '!./node_modules/esperanto/**/*', '!./node_modules/nwjs-release/**/*'], // use the glob format
+  // './node_modules/**/*', 
+  platforms: ['osx64', 'win32'],
   version: '0.12.3',
   buildDir: './build',
   zip: false
